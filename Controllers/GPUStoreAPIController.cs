b69cdb7a7583d7a0de59d9ea74856b95f04e270a
Rafał Nowak <rwojciechnowak@gmail.com>
2023-04-04 14:20:15 +0200

    feat: gpu model and basic get api controller

---
+++ Controllers/GPUStoreAPIController.cs
@@ -0,0 +1,22 @@
+﻿using GPUStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPUStoreAPI.Controllers
+{
+    [Route("api/GPUStoreAPI")]
+    [ApiController]
+    public class GPUStoreAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<GPU> GetGPUs() 
+        {
+            
+            return new List<GPU>
+            {
+                new GPU {ID = 1, Name="RTX 4090", Price=1599, Chip="AD102", Memory="24GB", MemoryType="GDDR6X" }, 
+                new GPU {ID = 2, Name="RX 6900 XT", Price=799, Chip="Navi21", Memory="16GB", MemoryType="GDDR6" }
+
+            };
+        }
+    }
+}
---
+++ Models/GPU.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GPUStoreAPI.Models
+{
+    public class GPU
+    {
+        [Required]
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Chip { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? MemoryType { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Memory { get; set; }
+    }
+}
